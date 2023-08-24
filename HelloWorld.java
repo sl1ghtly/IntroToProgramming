fa15df62ba7f2014ed8da8b56387aec43415aa69
sl1ghtly <83696921+sl1ghtly@users.noreply.github.com>
2023-08-24 15:52:41 +0100

    My first program

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+/* Practical 1- Question 1
+Eryk Gloginski
+Program to print text on screen*/
+
+public class HelloWorld
+{
+
+   public static void main(String [] args)
+   
+      {
+   
+      // Print the text to screen
+      System.out.print("Hello World");
+      
+      }
+   
+}
